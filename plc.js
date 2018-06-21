b0c97142517f2730198fc0a2fdfec156262571c3
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-06-20 19:33:58 -1000

    Done with proof of concept

--- plc.js
+++ plc.js
@@ -24,7 +24,8 @@ function Subject(name, times) {
   this.times = times;
 }
 
-subjects.push(new Subject("AP Physics", [[1, 2], [3, 4]]), new Subject("AP Chem", [[0, 1], [2, 3]]))
+subjects.push(new Subject("AP Physics", [[1, 2], [3, 4]]), 
+              new Subject("AP Chem", [[0, 1], [2, 3]]))
 
 console.log(subjects.find(function (sub) {
   return sub.name === "AP Physics"
@@ -37,7 +38,6 @@ function gt(ind) {
 }
 
 function generateGrid() {
-
   var row = $('<div></div>').attr('class', 'row');
   row.append($('<div></div>').attr('class', 'timecell'));
   for (var j = 0; j < 6; j++) {
@@ -51,15 +51,48 @@ function generateGrid() {
     row = $('<div></div>').attr('id', 'row-' + i).attr('class', 'row' + (i%2===0?' even':' odd'));
     row.append($('<div></div>').attr('class', 'timecell').html(gt(i)))
     for (var j = 0; j < 6; j++) {
-      var cell = $('<div></div>').attr('id', i + "-" + j).attr('class', 'col cell');
+      var cell = $('<div></div>').attr('id', j + "-" + i).attr('class', 'col cell');
       row.append(cell)
     }
     $('#t').append(row);
   }
 }
 
+//<a href="#" class="list-group-item list-group-item-action">Test</a>
 function generateMenu() {
+  for (var k = 0; k < subjects.length; k++) {
+    console.log(subjects[k])
+    $('#list').append($('<a></a>').attr('class', 'list-group-item list-group-item-action')
+                                  .html(subjects[k].name)
+                                  .on('click', function(e) {
+                                    e.preventDefault();
+                                    $('a').removeClass("active")
+                                    $(this).addClass("active")
+                                    highlight($(this).html())
+                                  }))
+  }
   
 }
 
+function highlight(name) {
+  clear();
+  var times = subjects.find(function (sub) {
+    return sub.name === name;
+  }).times
+
+
+  for (var k = 0; k < times.length; k++) {
+    $('#' + times[k][0] + '-' + times[k][1]).addClass('highlighted')
+  }
+}
+
+function clear() {
+  for (var k = 0; k < 9; k++) {
+    for (var m = 0; m < 6; m++) {
+      $('#' + k + '-' + m).removeClass('highlighted');
+    }
+  }
+}
+
 generateGrid()
+generateMenu()
